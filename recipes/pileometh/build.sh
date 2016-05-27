6812d132abb661bc6d17d1ca6c035bae5f275894
Devon Ryan <dpryan79@gmail.com>
2016-05-27 22:33:54 +0200

    Change test command and try to get zlib included via environment variables

--- recipes/pileometh/build.sh
+++ recipes/pileometh/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 make OPTS="-Wall -g -O3 -I$PREFIX/include -L$PREFIX/lib"
 cp PileOMeth $PREFIX/bin && chmod +x $PREFIX/bin/PileOMeth
