1ae06997b8e8daab576bddea04c86d4af1f619df
Devon Ryan <dpryan79@gmail.com>
2016-05-27 22:42:54 +0200

    What if we build htslib outside of PileOMeth's Makefile...

--- recipes/pileometh/build.sh
+++ recipes/pileometh/build.sh
@@ -3,5 +3,6 @@ set -eu -o pipefail
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
+make -C htslib
 make OPTS="-Wall -g -O3 -I$PREFIX/include -L$PREFIX/lib"
 cp PileOMeth $PREFIX/bin && chmod +x $PREFIX/bin/PileOMeth
