a41a75ab3ec46a6815e43ba5ee4595a4f4b93d4b
daler <dalerr@niddk.nih.gov>
2016-07-26 12:10:53 -0400

    trying to get paths to work

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -9,11 +9,16 @@ then
 else
     tag=MacOSX
 fi
-
+echo $(which python)
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
 sudo bash Miniconda3-latest-${tag}-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
 export PATH=/anaconda/bin:$PATH
+echo $(which python)
+
+hash -r
+
+echo $(which python)
 
 conda install -y --file scripts/requirements.txt
 
